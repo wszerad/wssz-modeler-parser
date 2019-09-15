d8eb33bf40c4d7917fc34bc73a3e26d9dea1daf7
Wszerad <wszerad91@gmail.com>
2019-09-15 20:49:01 +0200

    fix name

--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ import { getMarkers } from '@wssz/modeler';
 export { Parse, ItemsParse } from './src/decorators';
 
 const parserCache = new Map<any, Function>();
-export class ModelerParse {
+export class ModelerParser {
 	static parse(model: Function, source: Object, options: ModelerParserOptions = {}) {
 		if (!parserCache.has(model)) {
 			parserCache.set(model, new Model(model, getMarkers(model), options).execute());
 		}
 
-		return parserCache.get(model)(source, model, helpersCache, ModelerParse.parse);
+		return parserCache.get(model)(source, model, helpersCache, ModelerParser.parse);
 	}
 }
